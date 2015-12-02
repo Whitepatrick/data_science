4ee950386901fef5c1735b94648fac334b801d58
Patrick White <whitep@vcu.edu>
2015-12-01 21:33:30 -0500

    commiting

--- mac_ipython_start.sh
+++ mac_ipython_start.sh
@@ -8,6 +8,7 @@ docker build -t data-notebook docker/.
 docker run -d \
   -p 443:8888 \
   -v $HOME/workspace/data_science:/notebooks \
+  -v $HOME/workspace/DAT-NYC-29:/notebooks/notebooks/DAT-NYC-29 \
   -e "PASSWORD=data_science" \
   --name data-notebook \
   --hostname data-notebook \
---
+++ notes.md
@@ -0,0 +1,7 @@
+# statistics notes lesson 4
+
+12.15 discuss project idea
+12.22 
+1.14
+
+2 six sided die distribution b= 36
