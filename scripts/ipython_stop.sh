bb3be21cb079a4799dcbe1140da6c09249766cbc
Patrick White <whitep@vcu.edu>
2016-01-19 21:27:07 -0500

    adding lesson 15 stuff

--- scripts/ipython_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $HOME/workspace/data_science
-sudo docker stop data-notebook && sudo docker rm data-notebook
--- scripts/mac_ipython_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $HOME/workspace/data_science
-docker stop data-notebook && docker rm data-notebook
